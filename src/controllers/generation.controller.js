32e77a59ba08eb250e24c747879e3241578e748e
Darshan Varade <132351325+DarshanVarade@users.noreply.github.com>
2025-08-22 03:07:50 +0530

    add updates

--- src/controllers/generation.controller.js
+++ src/controllers/generation.controller.js
@@ -8,7 +8,7 @@ import { GoogleGenerativeAI } from "@google/generative-ai";
 
 // Initialize the Google Generative AI client
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
-const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
+const model = genAI.getGenerativeModel({ model: "gemini-2.0-flash" });
 
 const generateSummary = asyncHandler(async (req, res) => {
   const { materialId } = req.params;
