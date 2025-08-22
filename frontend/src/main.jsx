f45c9b5fe51d2f2efde0e626e92b1c18f6ceaa07
Darshan Varade <132351325+DarshanVarade@users.noreply.github.com>
2025-08-22 16:43:12 +0530

    update

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+// --- SWAP THESE TWO LINES ---
+import "./index.css"; // Import this first
+import "./App.css"; // Import this second
+// --------------------------
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
     <App />
-  </StrictMode>,
-)
+  </React.StrictMode>
+);
